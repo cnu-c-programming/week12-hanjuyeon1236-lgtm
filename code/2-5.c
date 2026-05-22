eee753bbb0612cfee008018fca0c4cbb715492ee
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-22 09:49:24 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,11 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], );
+    FILE* fp_dst = fopen(argv[2], );
 
+
+    fclose(fp_src);
+    fclose(fp_dst);
 }
 
