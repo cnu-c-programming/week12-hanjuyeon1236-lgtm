826f85c72a57644053baba2cd06bd1b58eb24887
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-22 09:45:34 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,22 @@
 #include <string.h>
 
 int main(int argc, const char* argv[], const char* envp[]) {
+    FILE* fp = fopen("student.txt", );
+
+      while (1) {
+          char name[10] = {0};
+          int score = 0;
+          scanf("%s", name);
+          if(strcmp(name, "quit") == 0)
+              break;
+          scanf("%d", &score);
+
+            (fp, "%s %d\n", name, score);
+      }
+
+    fclose(fp);
+    return 0;
+}
 
     return 0;
 }
