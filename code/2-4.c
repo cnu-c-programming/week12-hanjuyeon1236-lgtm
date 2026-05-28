f0fd416d97964709627e8da02ef815f13e6da747
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-28 22:42:14 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,23 +6,22 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
-   if (fp != NULL) {
-        int count[26] = {0};
-        int c;
+  if (fp == NULL)
+        return 0;
 
-        while ((c = fgetc(fp)) != EOF) {
-            if (c >= 'a' && c <= 'z') {
-                count[c - 'a']++;
-            }
+    int count[26] = {0};
+    int c;
+
+    while ((c = fgetc(fp)) != EOF) {
+
+        if (c >= 'a' && c <= 'z') {
+            count[c - 'a']++;
         }
+    }
 
-        for (int i = 0; i < 26; i++) {
-            if (count[i] > 0) {
-                printf("%c: %d\n", 'a' + i, count[i]);
-            }
-        }
-       fclose(fp);
-}
-return 0;
-}
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
 
+    fclose(fp);
+}
