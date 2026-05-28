d1e7636936760f6b404a356581154c9985939fa2
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-28 21:55:56 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -4,15 +4,20 @@ int main(int argc, const char* argv[]) {
     const char* filename = "myfile.txt";
 
     FILE* fp1 = fopen(filename, "r");
-    FILE* fp2 = fopen(filename, "w");
+    if (fp1 == NULL) return 1;
 
     char buf[256];
     while(fgets(buf, sizeof(buf), fp1) != NULL);
 
     char buf_fp1[256] = {0};
-    char buf_fp2[256] = {0};
 
     char* res_fp1 = fgets(buf_fp1, sizeof(buf_fp1), fp1);
+    FILE* fp2 = fopen(filename, "w");
+    if (fp2 == NULL) {
+        fclose(fp1);
+        return 1;
+    }
+    char buf_fp2[256] = {0};
     char* res_fp2 = fgets)buf_fp2, sizeof(buf_fp2), fp2);
 
     printf("fp1 %s %p %d %d\n", buf_fp1, res_fp1, ferror(fp1), feof(fp1));
