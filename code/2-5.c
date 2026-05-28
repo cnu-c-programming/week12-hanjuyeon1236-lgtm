e64f676ec8d822902b0ca5593c47532eb7831b20
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-28 21:46:33 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,10 +4,17 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
-    FILE* fp_src = fopen(argv[1], );
-    FILE* fp_dst = fopen(argv[2], );
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    if (fp_src == NULL || fp_dst == NULL) {
+        return 0;
+    }
 
+    int c;
+    while ((c = fgetc(fp_src)) != EOF) {
+        fputc(c, fp_dst);
+    }
     fclose(fp_src);
     fclose(fp_dst);
 }
